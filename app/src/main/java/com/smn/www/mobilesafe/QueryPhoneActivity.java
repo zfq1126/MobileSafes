8ff37df31bd4cda969cd95f1b06ff187a76c0c87
zfq <zfq0314@163.com>
2018-04-06 18:46:03 +0800

    重新增加备注000000000000

--- app/src/main/java/com/smn/www/mobilesafe/QueryPhoneActivity.java
+++ app/src/main/java/com/smn/www/mobilesafe/QueryPhoneActivity.java
@@ -45,7 +45,7 @@ public class QueryPhoneActivity extends Activity {
             /*
             *
             * 学会用git 和android studio  还有idea
-            *
+            *22222222222222
             * */
             @Override
             public void afterTextChanged(Editable s) {
