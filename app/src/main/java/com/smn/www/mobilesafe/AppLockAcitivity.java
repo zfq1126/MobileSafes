033636b16239f4e61ed87777c1ddae59c1ef4f63
zfq <zfq0314@163.com>
2018-04-08 15:25:20 +0800

    4-8创建引擎类为程序锁页面提供数据， 并创建加锁程序数据库，用来存放加锁的应用程序。

--- app/src/main/java/com/smn/www/mobilesafe/AppLockAcitivity.java
+++ app/src/main/java/com/smn/www/mobilesafe/AppLockAcitivity.java
@@ -3,9 +3,17 @@ package com.smn.www.mobilesafe;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+
+import com.smn.www.mobilesafe.bean.AppInfo;
+import com.smn.www.mobilesafe.db.AppInfoDao;
+import com.smn.www.mobilesafe.engine.AppInfoProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -23,12 +31,39 @@ public class AppLockAcitivity extends Activity {
     ListView lvUnlock;
     @InjectView(R.id.lv_lock)
     ListView lvLock;
+    private List<AppInfo> unLockList;
+    private List<AppInfo> lockList;
+    private AppInfoDao dao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_lock);
         ButterKnife.inject(this);
+
+        dao = new AppInfoDao(this);
+        List<AppInfo> appInfos = AppInfoProvider.AppInfoList(this);
+        //遍历已将安装城程序的所有信息
+        for (AppInfo appInfo:appInfos) {
+            // 需要检测这些应用程序是加锁的还是没有加锁
+            // 需要一个已加锁的 数据库,专门用来 对是否加锁应用程序信息的 增 删 查
+            // 如何判断 属于已加锁 的还是 未加锁的 : 通过包名在数据库中查询 ,如果查到,说明属于已经加锁的 应用,否则属于没有加锁的应用
+
+            //未加锁的集合
+            unLockList = new ArrayList<>();
+            //已加锁的集合
+            lockList = new ArrayList<>();
+            boolean b = dao.QueryPackagename(appInfo.getPackageName());
+            if (b){
+                // 说明属于已经加锁的应用   将这些应用放在一个已加锁的应用集合中
+                lockList.add(appInfo);
+            }else {
+                // 说明属于已经加锁的应用   将这些应用放在一个未加锁的应用集合中
+                unLockList.add(appInfo);
+            }
+            Log.i("zzzzzzzzz",appInfo.toString());
+        }
+        //4-9开始用适配器适配两个listView，进行数据的填充
     }
 
     @OnClick({R.id.bt_unlock, R.id.bt_lock})
@@ -38,10 +73,10 @@ public class AppLockAcitivity extends Activity {
                 // 当点击 未加锁的时候  未加锁的 背景 变为紫色 文字颜色为白色
                 // 已加锁 背景变为 白色  文字颜色变为紫色，
                 //修改关于背景颜色的设置
-                btUnlock.setBackgroundResource(R.drawable.shape_unlock_write_bg);
+                btUnlock.setBackgroundResource(R.drawable.shape_unlock_purple_bg);
                 btUnlock.setTextColor(Color.WHITE);
 
-                btLock.setBackgroundResource(R.drawable.shape_lock_purple_bg);
+                btLock.setBackgroundResource(R.drawable.shape_lock_write_bg);
                 btLock.setTextColor(Color.BLUE);
                 lvUnlock.setVisibility(View.VISIBLE);
                 lvLock.setVisibility(View.GONE);
@@ -49,8 +84,6 @@ public class AppLockAcitivity extends Activity {
             case R.id.bt_lock:
                 // 当点击 已加锁的时候  已加锁的 背景 变为紫色 文字颜色为白色
                 // 未加锁 背景变为 白色  文字颜色变为紫色
-                //这部分还有问题的
-                //需要重新理解，修改
                 btLock.setBackgroundResource(R.drawable.shape_lock_purple_bg);
                 btLock.setTextColor(Color.WHITE);
 
